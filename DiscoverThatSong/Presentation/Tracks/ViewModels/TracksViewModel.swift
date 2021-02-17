0975de91d42fd2d4b744acc3499f5d8a340072b2
Mohammed Abdullatif <mabdullatif@spotify.com>
2021-02-17 11:42:56 +0100

    Improve the implementation of "TracksViewModel".

--- DiscoverThatSong/Presentation/Tracks/ViewModels/TracksViewModel.swift
+++ DiscoverThatSong/Presentation/Tracks/ViewModels/TracksViewModel.swift
@@ -8,19 +8,41 @@
 import Combine
 import Foundation
 
-struct TracksViewModel {
+class TracksViewModel {
 
-    // MARK: - Properties
+    // MARK: - Internal
 
-    private let tracksDataSource: TracksDataSource
+    @Published var tracks: [Track] = []
 
     // MARK: - Initializer
 
     init(tracksDataSource: TracksDataSource) {
         self.tracksDataSource = tracksDataSource
+        fetchTracks()
     }
 
-    var tracks: AnyPublisher<Tracks, Error> {
-        return tracksDataSource.tracks(ids: "3n3Ppam7vgaVa1iaRUc9Lp,3twNvmDtFQtAd5gMKedhLD")
+    private func fetchTracks() {
+        tracksDataSource
+            .tracks(ids: "3n3Ppam7vgaVa1iaRUc9Lp,3twNvmDtFQtAd5gMKedhLD")
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] completion in
+                guard let self = self else { return }
+                switch completion {
+                case .failure(let error):
+                    self.tracks = []
+                    print("ERROR: An error happened while fetching the tracks: \(error.localizedDescription)")
+                case .finished:
+                    break
+                }
+            } receiveValue: { [weak self] tracks in
+                guard let self = self else { return }
+                self.tracks = tracks.tracks
+            }.store(in: &disposables)
+
     }
+
+    // MARK: - Private
+
+    private let tracksDataSource: TracksDataSource
+    private var disposables = Set<AnyCancellable>()
 }
